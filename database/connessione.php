c84afd220bff1627bb133ec00c317785984812d3
mrpeppo47 <155762498+mrpeppo47@users.noreply.github.com>
2025-04-17 15:44:30 +0200

    Create connessione.php

---
+++ database/connessione.php
@@ -0,0 +1,12 @@
+<?php
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASS");
+$dbname = getenv("DB_NAME");
+
+$connessione = new mysqli($host, $user, $password, $dbname);
+
+if ($connessione->connect_error) {
+    die("Connessione fallita: " . $connessione->connect_error);
+}
+?>
