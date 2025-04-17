28b8f979d325454e195fa8edcab52975d5ee0e42
mrpeppo47 <155762498+mrpeppo47@users.noreply.github.com>
2025-04-17 17:17:34 +0200

    Update connessione.php

--- database/connessione.php
+++ database/connessione.php
@@ -1,10 +1,11 @@
 <?php
 $host = getenv("DB_HOST");
+$port = getenv("DB_PORT");
 $user = getenv("DB_USER");
 $password = getenv("DB_PASS");
 $dbname = getenv("DB_NAME");
 
-$connessione = new mysqli($host, $user, $password, $dbname);
+$connessione = new mysqli($host, $user, $password, $dbname, $port);
 
 if ($connessione->connect_error) {
     die("Connessione fallita: " . $connessione->connect_error);
